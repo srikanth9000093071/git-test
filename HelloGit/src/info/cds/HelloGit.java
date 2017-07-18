bd292aed843dfa51456b4c8456ec50dee6dea65a
Satya <botta.satyanarayana@gmail.com>
2017-07-18 16:00:55 +0530

    added new names

--- HelloGit/src/info/cds/HelloGit.java
+++ HelloGit/src/info/cds/HelloGit.java
@@ -3,10 +3,11 @@ package info.cds;
 public class HelloGit {
 	
 	public static void main(String[] args) {
-		String[] names={"Srikanth","Satya","Koti","Vijay","Surya"};
+		String[] names={"Srikanth","Satya","Koti","Vijay","Surya","Gopi"};
 		// a greeting
 		for (int i = 0; i < names.length; i++) {
 			System.out.println("Good Morning "+names[i]);
+			System.out.println("Good Afternoon:");
 		}
 		
 	}
