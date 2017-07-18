c994f9f8a7dd217f687c4fdedb9a807505c57803
Srikanth Machavaram <srikanth9000093071@gmail.com>
2017-07-18 15:58:54 +0530

    changed for loop to for-each loop to increase performance

--- HelloGit/src/info/cds/HelloGit.java
+++ HelloGit/src/info/cds/HelloGit.java
@@ -4,11 +4,10 @@ public class HelloGit {
 	
 	public static void main(String[] args) {
 		String[] names={"Srikanth","Satya","Koti","Vijay","Surya"};
-		// a greeting
-		for (int i = 0; i < names.length; i++) {
-			System.out.println("Good Morning "+names[i]);
+		// a greeting & with new Loop
+		for (String name : names) {
+			System.out.println("Good morning "+name);
 		}
-		
 	}
 
 }
